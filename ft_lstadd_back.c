b237b114a9d398d2bd570a5b3f1cc8f9c984917a
surajbartoula <155395336+surajbartoula@users.noreply.github.com>
2024-03-30 23:47:39 +0400

    Update ft_lstadd_back.c

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -12,7 +12,7 @@
 
 #include "libft.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+void	ft_lstadd_back(t_list **lst, t_list *new_lst)
 {
 	t_list	*node;
 
@@ -21,8 +21,8 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 		node = *lst;
 		while (node->next)
 			node = node->next;
-		node->next = new;
+		node->next = new_lst;
 	}
 	else
-		*lst = new;
+		*lst = new_lst;
 }
